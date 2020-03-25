54d11b5d6b73499424f309f6014a97a101cdfbae
Jofiag <Aguemon.wahab.97@gmail.com>
2020-03-25 11:29:18 +0100

    Initial commit

---
+++ app/src/main/java/com/jofiagtech/listfragment/MainActivity.java
@@ -0,0 +1,24 @@
+package com.jofiagtech.listfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.container);
+
+        if (fragment == null){
+            fragment = new MainFragment();
+            fragmentManager.beginTransaction().add(R.id.container, fragment).commit();
+        }*/
+    }
+}
