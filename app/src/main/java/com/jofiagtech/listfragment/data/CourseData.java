f31b633eb2e82cd171fa4adcfa67b7aad5b4faff
Jofiag <Aguemon.wahab.97@gmail.com>
2020-03-26 10:56:42 +0100

    Creation of a course data class to get a course list.

---
+++ app/src/main/java/com/jofiagtech/listfragment/data/CourseData.java
@@ -0,0 +1,21 @@
+package com.jofiagtech.listfragment.data;
+
+import java.util.ArrayList;
+
+public class CourseData {
+    private String[] courseNames = {"First Course", "Second Course", "Third Course", "Fourth Course",
+            "Fifth Course", "Sixth Course", "Seventh Course"};
+
+    public ArrayList<Course> getCourseList(){
+        ArrayList<Course> list = new ArrayList<>();
+
+        for (String courseName : courseNames) {
+            Course course = new Course(courseName,
+                    courseName.replace(" ", "").toLowerCase());
+
+            list.add(course);
+        }
+
+        return list;
+    }
+}
