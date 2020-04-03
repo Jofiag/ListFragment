c143a23258de4d6f701cd5d696ab62f7db22ff5f
Jofiag <Aguemon.wahab.97@gmail.com>
2020-04-03 14:09:31 +0200

    Setup Course details activity and fragment.

--- app/src/main/java/com/jofiagtech/listfragment/activities/CourseDetailsActivity.java
+++ app/src/main/java/com/jofiagtech/listfragment/activities/CourseDetailsActivity.java
@@ -1,10 +1,13 @@
 package com.jofiagtech.listfragment.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import com.jofiagtech.listfragment.R;
+import com.jofiagtech.listfragment.fragment.CourseDetailsFragment;
 
 public class CourseDetailsActivity extends AppCompatActivity {
 
@@ -12,5 +15,21 @@ public class CourseDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
+
+        if (savedInstanceState == null){
+            addFragment(new CourseDetailsFragment(), R.id.course_details_container);
+        }
+    }
+
+    private void addFragment(Fragment fragmentToAdd, int container){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(container);
+
+        if (fragment == null){
+            fragment = fragmentToAdd;
+            fragmentManager.beginTransaction()
+                    .add(container, fragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/jofiagtech/listfragment/fragment/CourseDetailsFragment.java
+++ app/src/main/java/com/jofiagtech/listfragment/fragment/CourseDetailsFragment.java
@@ -1,6 +1,40 @@
 package com.jofiagtech.listfragment.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.jofiagtech.listfragment.R;
+import com.jofiagtech.listfragment.model.Course;
+
 public class CourseDetailsFragment extends Fragment {
+    Course mCourse;
+
+    public CourseDetailsFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.course_details_fragment, container, false);
+
+        if (mCourse != null){
+            TextView courseName = view.findViewById(R.id.details_name);
+            TextView courseDescription = view.findViewById(R.id.details_description);
+            ImageView courseImage = view.findViewById(R.id.details_image);
+
+            courseName.setText(mCourse.getCourseName());
+            courseDescription.setText(mCourse.getCourseName());
+            courseImage.setImageResource(mCourse.getImageRessourceId(getActivity()));
+        }
+
+        return view;
+    }
 }
